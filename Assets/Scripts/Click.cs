fcd6279a324e26e4b87b1c25b708175146f04f27
Andras-corda <220672@site.asty-moulin.be>
2025-04-04 16:16:13 +0200

    -

--- Assets/Scripts/Click.cs
+++ Assets/Scripts/Click.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Click : MonoBehaviour
 {
     public int score = 0;
+    public TextMeshProUGUI scoreText;
+
+    public void Start()
+    {
+        UpdateScoreText();
+    }
 
     public void OnClickButton()
     {
         score++;
-        Debug.Log("Score: " + score);
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = "Score : " + score.ToString();
     }
 }
