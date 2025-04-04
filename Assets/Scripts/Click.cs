8b41c22a90771ca17fd934b2b83236a4d2848aee
Andras-corda <220672@site.asty-moulin.be>
2025-04-04 16:09:22 +0200

    -

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public int score = 0;
+
+    public void OnClickButton()
+    {
+        score++;
+        Debug.Log("Score: " + score);
+    }
+}
